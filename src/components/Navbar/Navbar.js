9b9d4f7d61ebc2849ad0e8f90661f88b3ee5f785
sharvani3 <sharvaniyedla11@gmail.com>
2024-12-23 15:22:15 +0530

    the final edit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,17 +7,17 @@ export default function Navbar({ handlesetsearch, handlesetcategoryvalue, handle
 
   return (
     <>
-      <div className="navbar justify-content-evenly position-sticky top-0 d-flex">
+      <div className="navbar justify-content-evenly position-relative top-0 d-flex">
         <div className="d-flex align-items-center">
           <img src={logo} alt="logo" className="px-4" />
-          <Link onClick={handleclearfilter} to='/' style={{ textDecoration: "none", color: "white", fontFamily: "Lucida handwriting,Time New Roman" }}><h3 className="navbarHeading">Humari Dukaan</h3></Link>
+          <Link onClick={handleclearfilter} to='/' style={{  color: "black", fontFamily: "Lucida handwriting,Time New Roman" }}><h3 className="navbarHeading">Mytalorzone by sahiba</h3></Link>
         </div>
         <div className="d-flex align-items-center border-bottom border-light">
-          <input onChange={handlesetsearch} className=" mx-3 bg-transparent border-0 search" id="search" style={{ height: "30px", width: "" }} placeholder="Seach items" />
+          <input onChange={handlesetsearch} className=" mx-3 bg-transparent border-0 search" id="search" style={{ color:"black", height: "30px", width: "" }} placeholder="Seach items" />
           <h4><i className="bi bi-search"></i></h4>
         </div>
         <div className="d-flex align-items-center">
-          <Link to="/checkout/info" style={{ textDecoration: "none", color: "white" }}>
+          <Link to="/checkout/info" style={{ textDecoration: "none", color: "black" }}>
             <h1><i className="bi bi-cart3"></i></h1></Link>
         </div> 
       </div>
@@ -29,16 +29,16 @@ export default function Navbar({ handlesetsearch, handlesetcategoryvalue, handle
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 justify-content-evenly" style={{ paddingLeft: '1rem', paddingRight: '1rem', width: "100%" }}>
               <li className="nav-item">
-                <div className="nav-link fs-5 p-1 px-5 smnvit" onClick={handlesetcategoryvalue} >Women's clothing</div>
+                <div className="nav-link fs-5 p-1 px-5 smnvit" onClick={handlesetcategoryvalue} >Ethnic Wear</div>
               </li>
               <li className="nav-item">
-                <div className="nav-link fs-5 p-1 px-5 smnvit" aria-current="page" onClick={handlesetcategoryvalue}>Men's clothing</div>
+                <div className="nav-link fs-5 p-1 px-5 smnvit" aria-current="page" onClick={handlesetcategoryvalue}>Westren wear</div>
               </li>
               <li className="nav-item">
-                <div className="nav-link fs-5 p-1 px-5 smnvit" onClick={handlesetcategoryvalue}>Jewelery</div>
+                <div className="nav-link fs-5 p-1 px-5 smnvit" onClick={handlesetcategoryvalue}>Sahiba Exclusives</div>
               </li>
               <li className="nav-item">
-                <div className="nav-link fs-5 p-1 px-5 smnvit" onClick={handlesetcategoryvalue}>Electronics</div>
+                <div className="nav-link fs-5 p-1 px-5 smnvit" onClick={handlesetcategoryvalue}>Winter Collection</div>
               </li>
             </ul>
           </div>
@@ -48,17 +48,3 @@ export default function Navbar({ handlesetsearch, handlesetcategoryvalue, handle
     </>
   )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
